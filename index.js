2f26dd9e6dd6c8e3a1f0b5999caa5bcf09668611
carolina <ci.arayagonzalez@gmail.com>
2024-11-14 12:29:48 -0300

    add path to get data

--- index.js
+++ index.js
@@ -5,11 +5,17 @@ import {
   eliminarActores,
 } from "./database/actor.js";
 import { createServer } from "http"
+import url from "url"
 
 const port = 3000
 
 const server = createServer((req, res)=>{
+    const method = req.method
+    const urlParsed = url.parse(req.url, true)
+    console.log(urlParsed)
 
+    res.setHeader("Content-Type", "application/json")
+    res.end(JSON.stringify({ message: "Respuesta a petición" }))
 })
 server.listen(port, ()=> console.log(`Servidor escuchando por el puerto ${port}`))
 
